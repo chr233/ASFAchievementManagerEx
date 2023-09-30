5b36a903ca5a8dd3ee160e590be2c5e5ea970dff
Chr_ <chr@chrxw.com>
2023-09-30 18:43:58 +0800

    update

---
+++ ASFAchievementManagerEx/Data/StatFlags.cs
@@ -0,0 +1,10 @@
+﻿namespace ASFAchievementManagerEx.Data;
+
+[Flags]
+internal enum StatFlags
+{
+    None = 0,
+    IncrementOnly = 1 << 0,
+    Protected = 1 << 1,
+    UnknownPermission = 1 << 2,
+}
--- ASFAchievementManagerEx/Data/StatInfo.cs
+++ ASFAchievementManagerEx/Data/StatInfo.cs
@@ -22,13 +22,3 @@ internal record IntStatInfo
     }
 
 }
-
-
-[Flags]
-internal enum StatFlags
-{
-    None = 0,
-    IncrementOnly = 1 << 0,
-    Protected = 1 << 1,
-    UnknownPermission = 1 << 2,
-}
